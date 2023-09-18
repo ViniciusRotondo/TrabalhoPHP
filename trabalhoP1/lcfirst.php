64448f6c8e2e1704f61b1df8fdb40605dd2c349c
Vinícius Rotondo <vinirs.trade@gmail.com>
2023-09-18 13:42:51 -0300

    First Commit

---
+++ trabalhoP1/lcfirst.php
@@ -0,0 +1,29 @@
+<?php
+    if (isset($_POST["text"])) {
+        $text = $_POST["text"];
+        $textmod = lcfirst($text);
+
+    } else {
+        $text = "";
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados do Formulário</title>
+</head>
+<body>
+    <h1>RESULTADO DO LCFIRST</h1>
+    <p>TEXTO DIGITADO: <?php echo $text; ?></p>
+    <p>TEXTO ALTERADO PELO LCFIRST:  <?php echo $textmod; ?></p>
+
+    
+
+    <a href="/trabalhoP1/index.html">Clique aqui para voltar para o Início</a>
+
+</body>
+</html>
